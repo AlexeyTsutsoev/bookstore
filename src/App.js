f2dd659fb9a6626179906b4f6b116645ad78e058
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-04 18:19:59 +0300

    transfer checkUser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BookStore from "./pages/BookStore";
 import store from "./store/store";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { checkUser } from "./api/user";
+import { setUser } from "./store/actionCreators/userAction";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const userHandler = async () => {
+    try {
+      const result = await checkUser();
+
+      dispatch(setUser(result.data.user));
+      localStorage.setItem("token", result.data.token);
+    } catch (err) {
+      console.log(err);
+      localStorage.removeItem("token");
+    }
+  };
+
+  useEffect(() => {
+    userHandler();
+  }, []);
   return (
     <Provider store={store}>
       <BrowserRouter>
