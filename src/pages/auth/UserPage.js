4f6b08b7dff06598a095254536fcb49e679b4b4d
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-11 11:06:39 +0300

    layout userpage

--- src/pages/auth/UserPage.js
+++ src/pages/auth/UserPage.js
@@ -1,7 +1,48 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import { signOut } from "../../store/actionCreators/userAction";
+
+const PageContainer = styled.main`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+`;
+
+const PageContent = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid black;
+`;
+
+const Photo = styled.div`
+  margin-right: 20px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const InfoItem = styled.div`
+  border-bottom: 1px solid black;
+`;
 
 const UserPage = () => {
   const user = useSelector((state) => state.user.user);
@@ -11,16 +52,27 @@ const UserPage = () => {
     dispatch(signOut());
   };
   return (
-    <div>
-      <div>id: {user.id} </div>
-      <div>name: {user.name} </div>
-      <div>email: {user.email} </div>
-      <div>avatar: {user.avatar ? <img src={user.avatar} /> : "no photo"} </div>
+    <PageContainer>
+      <PageContent>
+        <Photo>
+          {user.avatar ? (
+            <img src={user.avatar} />
+          ) : (
+            <img src={"https://place-hold.it/200x200"} />
+          )}
+        </Photo>
+        <UserInfo>
+          <InfoItem>id: {user.id} </InfoItem>
+          <InfoItem>name: {user.name} </InfoItem>
+          <InfoItem>email: {user.email} </InfoItem>
+          <NavLink to='/favor'>Ваши избранные книги </NavLink>
+          <button onClick={outHandler}>Выход</button>
+        </UserInfo>
+      </PageContent>
       <div>
         <NavLink to='/'>на главную </NavLink>
       </div>
-      <button onClick={outHandler}>Выход</button>
-    </div>
+    </PageContainer>
   );
 };
 
