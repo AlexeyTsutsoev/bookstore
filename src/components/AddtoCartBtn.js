affad8bdcfe6f41f0456f13a676e60f640704863
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-12 17:12:11 +0300

    add delete btn

--- src/components/AddtoCartBtn.js
+++ src/components/AddtoCartBtn.js
@@ -1,16 +1,47 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { addBook } from "../store/actionCreators/cartAction";
+import { addBook, deleteBook } from "../store/actionCreators/cartAction";
 
+//NEED REVIEW
 const AddToCartBtn = ({ book }) => {
   const dispatch = useDispatch();
+  const [isInCart, setInCart] = useState(false);
+
+  const checkCart = () => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    let result = false;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].book.id === book.id) {
+        result = true;
+      }
+    }
+    setInCart(result);
+  };
+
+  useEffect(() => {
+    checkCart();
+  }, [checkCart]);
 
   const addtoCart = (book) => {
     dispatch(addBook(book));
+    setInCart(true);
   };
 
-  return (
+  const removeFromCart = (id) => {
+    dispatch(deleteBook(id));
+    setInCart(false);
+  };
+
+  return isInCart ? (
+    <Button
+      variant='contained'
+      color='secondary'
+      onClick={() => removeFromCart(book.id)}
+    >
+      Удалить из корзины
+    </Button>
+  ) : (
     <Button variant='contained' color='primary' onClick={() => addtoCart(book)}>
       Добавить в корзину
     </Button>
