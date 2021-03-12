7de7a16300f949265ceb956bcc35c74b37b55c98
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-12 17:12:58 +0300

    remove redudant import

--- src/components/BooksArea.js
+++ src/components/BooksArea.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { getFavoritesFromDb } from "../api/favorites";
 import { loadBook } from "../store/actionCreators/booksAction";
 import BookItem from "./BookItem";
 import Pages from "./Pages";
