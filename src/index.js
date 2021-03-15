b44b7998651bef254d863f83bbb110e81bc1e103
Alexey Tsutsoev <imistersin@gmail.com>
2021-03-15 18:21:10 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import { Container } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Container>
+        <App />
+      </Container>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
